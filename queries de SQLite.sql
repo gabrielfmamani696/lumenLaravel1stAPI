623555eb788b7357e10f981796ed713ca0db23e1
gabrielQHome <gabrielfmamanicatari@gmail.com>
2025-06-28 21:44:20 -0400

    ¿Cómo CREAR una SIMPLE REST API con LARAVEL LUMEN PHP?

---
+++ queries de SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO users (name, email, password, phone, status, created_at, updated_at)
+VALUES ('gabriel2', 'algunos@hola.com', 'contra2', '654646', 1, datetime('now', 'localtime'), datetime('now', 'localtime'));
